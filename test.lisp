a7e7045a93c05468a5afb582493b572c38361659
Luke Beukelman <lbeuk@proton.me>
2023-11-17 17:13:24 +0000

    Some test cases

--- test.lisp
+++ test.lisp
@@ -10,5 +10,27 @@
 ;; test %DIST-OR-AND-AND
 ;; test dpll-unit-propagate
 ;; test dpll
+
+;; Individual test cases
 (test-case (sat-p '(and a b)) t)
+(test-case (sat-p '(and (not a) a)) nil)
+(test-case (sat-p '(or a b)) t)
+(test-case (sat-p '(:xor a b)) t)
+(test-case (sat-p '(:xor a a)) nil)
+(test-case (sat-p '(:xor (not a) a)) t)
+(test-case (sat-p '(:iff a a)) t)
+(test-case (sat-p '(:iff a b)) t)
+(test-case (sat-p '(:iff a (not a))) nil)
+(test-case (sat-p '(:implies a a)) t)
+
+;; Combined test cases
+;; Fails implies, a must be true and b must be false
+(test-case (sat-p '(and a (not b) (:implies a b))) nil)
+(test-case (sat-p '(and a (not b) (:xor a b))) t)
+(test-case (sat-p '(and a b (:xor a b))) nil)
+(test-case (sat-p '(and (and a b) (:xor a b))) nil)
 (test-case (sat-p '(and (or a) (or (not a)))) nil)
+; (test-case (sat-p '(and (:xor a b) (:iff a b))) nil)
+(test-case (sat-p '(or (:xor a b) (:iff a b))) t)
+(test-case (sat-p '(and a (and a (and a (and a (not a)))))) nil)
+(test-case (sat-p '(and a (and a (and a (and a a))))) t)
